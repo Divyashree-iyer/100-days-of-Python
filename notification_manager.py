8ef8e9917bf6bb99f601c639aa42ea7edbed6a0f
Divyashree-iyer <59418312+Divyashree-iyer@users.noreply.github.com>
2022-02-07 18:52:09 +0530

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -4,13 +4,13 @@ class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     
     def send_mssg(self, mssg):
-        account_sid ='ACfb4ae8eb10265c8cce54fe1afa1ef67a'
-        auth_token = '747eb166f683724ab8cee792ed756032'
+        account_sid =YOUR_SID
+        auth_token = YOUR_TOKEN
         client = Client(account_sid, auth_token)
         
         message = client.messages.create(
                                       body=mssg,
-                                      from_='+19033296495',
-                                      to='+917874683021'
+                                      from_=NUMBER,
+                                      to=NUMBER
                                   )
-        print(message.sid)
+        
