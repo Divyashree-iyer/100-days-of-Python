f4a350253fceea346aa9b75751301596a4a5ce63
Divyashree-iyer <59418312+Divyashree-iyer@users.noreply.github.com>
2022-02-07 18:52:52 +0530

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -6,16 +6,14 @@ class DataManager:
     
     def __init__(self):
         
-        self.url = 'https://api.sheety.co/0427e10ce9456848a20da460c2810e5f/flightSearch39/sheet1'
+        self.url = URL2SHEETY
         
     def get(self):
         self.res=requests.get(url=self.url)
         self.data=self.res.json()
         self.dataList=self.data['sheet1']
         return self.dataList
-        #print(self.res.json())
-        #print("response.status_code =", self.res.status_code)
-        #print("response.text= ", self.res.text)
+        
     
     def put(self, id, iataCode):
         putUrl=self.url+'/'+str(id)
